c60fc94d72f66eec452fbe0fc0ecb3a28367751b
Dzulfikar Adi Putra <dzulfikar.adiputra@gmail.com>
2025-03-29 21:56:11 +0700

    feat: basic svelte5 routing

---
+++ src/lib/MainMenu.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Link } from "svelte5-router";
+</script>
+
+<nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/dashboard">Dashboard</Link>
+</nav>
---
+++ src/page/Dashboard.svelte
@@ -0,0 +1,6 @@
+<script>
+  import MainMenu from "../lib/MainMenu.svelte";
+</script>
+
+<MainMenu/>
+<h1>Dashboard</h1>
